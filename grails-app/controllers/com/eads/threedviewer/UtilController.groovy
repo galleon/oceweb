7a69dd045f557bc5a9e9b84b8be290ab6536a755
Uday Pratap <uday@intelligrape.com>
2012-01-03 14:54:47 +0530

    Added utility action to generate brep file for cube

--- grails-app/controllers/com/eads/threedviewer/UtilController.groovy
+++ grails-app/controllers/com/eads/threedviewer/UtilController.groovy
@@ -1,8 +1,16 @@
 package com.eads.threedviewer
 
+import org.jcae.opencascade.jni.BRepPrimAPI_MakeBox
+import org.jcae.opencascade.jni.BRepTools
+
 class UtilController {
 
     def index() {
+        double[] p1 = [5, 5, 5]
+        double[] p2 = [-5, -5, -5]
 
+        BRepPrimAPI_MakeBox box = new BRepPrimAPI_MakeBox(p1, p2)
+        BRepTools.write(box.shape(), "difference.brep")
+        render "success"
     }
 }
