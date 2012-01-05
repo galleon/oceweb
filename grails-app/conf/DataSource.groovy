d08cddd8d5924b1f1133468099b447a28c59b265
Uday Pratap <uday@intelligrape.com>
2012-01-05 11:09:41 +0530

    Updated datasource for qa env

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,43 +1,58 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = org.hibernate.dialect.MySQL5InnoDBDialect // must be set for transactions to work!
+    dbCreate = "update"
+    url = "jdbc:mysql://localhost:3306/3d_viewer?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+    username = "root"
+    password = "igdefault"
+    properties {
+        maxActive = 50
+        maxIdle = 25
+        minIdle = 10
+        initialSize = 20
+        numTestsPerEvictionRun = 3
+        testOnBorrow = true
+        testWhileIdle = true
+        testOnReturn = true
+        validationQuery = "SELECT 1"
+        minEvictableIdleTimeMillis = (1000 * 60 * 5)
+        timeBetweenEvictionRunsMillis = (1000 * 60 * 5)
+    }
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
+
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
-        }
-    }
-    test {
-        dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+            url = "jdbc:mysql://localhost:3306/3d_viewer?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
         }
     }
+
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            url = "jdbc:mysql://localhost:3306/3d_viewer?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+        }
+    }
+
+    qa {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost:3306/3d_viewer?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+        }
+    }
+
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost:3306/3d_viewer?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
         }
     }
 }
