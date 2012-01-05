af6d911c11abab7dc78f8e0afe5da1b850ac15ee
Uday Pratap <uday@intelligrape.com>
2012-01-05 14:23:10 +0530

    Removed resources plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,13 +33,12 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
-        // runtime 'mysql:mysql-connector-java:5.1.16'
+        runtime 'mysql:mysql-connector-java:5.1.16'
     }
 
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
-        runtime ":resources:1.1.5"
 
         build ":tomcat:$grailsVersion"
     }
