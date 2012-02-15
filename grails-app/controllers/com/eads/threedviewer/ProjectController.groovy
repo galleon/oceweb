cb02c91e3cea31f6bfd5820047d648709ec4a0e3
Uday Pratap Singh <uday@intelligrape.com>
2012-02-15 12:16:55 +0530

    Fixed Bug: Project changes when new shape is created

--- grails-app/controllers/com/eads/threedviewer/ProjectController.groovy
+++ grails-app/controllers/com/eads/threedviewer/ProjectController.groovy
@@ -4,17 +4,20 @@ class ProjectController {
 
     def index(String name) {
         List<Project> projects = Project.list()
-        [projects: projects, project: name ? Project.findOrSaveWhere([name: name]) : (projects ? projects.first() : null),shapeId:params.shapeId]
+        CADObject cadObject = params.shapeId ? CADObject.get(params.shapeId) : null
+        Project project = cadObject ? cadObject.project : null
+        project = name ? Project.findOrSaveWhere([name: name]) : project
+        [projects: projects, project: project ?: (projects ? projects.first() : null), shapeId: params.shapeId]
     }
 
     def listCadObjects(Long id) {
         Project project = id ? Project.read(id) : null
         render(template: 'cadObjects', model: [project: project, projects: Project.list()])
     }
-    
-    def delete(String name, Long id){
+
+    def delete(String name, Long id) {
         Project project = id ? Project.get(id) : null
-        if (project){
+        if (project) {
             project.delete()
         }
         redirect(action: 'index')
