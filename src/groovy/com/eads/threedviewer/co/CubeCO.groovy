0de14c32bd64cf8d7db62321e89ef7c21118486f
Rahul <rahul@intelligrape.com>
2012-02-16 13:44:08 +0530

    error fixed for creating cube

--- src/groovy/com/eads/threedviewer/co/CubeCO.groovy
+++ src/groovy/com/eads/threedviewer/co/CubeCO.groovy
@@ -6,6 +6,7 @@ import org.jcae.opencascade.jni.BRepPrimAPI_MakeBox
 import org.jcae.opencascade.jni.TopoDS_Shape
 import com.eads.threedviewer.CADObject
 import com.eads.threedviewer.CADCubeObject
+import com.eads.threedviewer.enums.ShapeType
 
 @Validateable
 @ToString(includeNames = true, includeFields = true, excludes = 'metaClass,errors', includeSuper = true)
@@ -25,7 +26,7 @@ class CubeCO extends ShapeCO {
     }
 
     CADObject getCADObject() {
-        CADObject cadObject = new CADCubeObject(name: name, project: project, x: x, y: y, z: z, x1: x1, y1: y1, z1: z1, content: content)
+        CADObject cadObject = new CADCubeObject(name: name, project: project, x: x, y: y, z: z, x1: x1, y1: y1, z1: z1, content: content, type: ShapeType.CUBE)
         return cadObject
     }
 
