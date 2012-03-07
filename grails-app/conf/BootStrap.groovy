850714f6c03866b21bd5c7839acfdbc3ee929023
Uday Pratap Singh <uday@intelligrape.com>
2012-03-07 11:59:38 +0530

    Removed unused code

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,13 +1,8 @@
-import com.eads.threedviewer.util.MetaClassHelper
-
 class BootStrap {
 
     def bootStrapService
 
     def init = { servletContext ->
-        MetaClassHelper.enrichClasses()
-
-//        bootStrapService.bootstrapData()
     }
     def destroy = {
 
