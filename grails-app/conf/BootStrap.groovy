a1e3b81c566e10c353a930c0b5f96967710eda3a
Uday Pratap Singh <uday@intelligrape.com>
2012-04-17 15:24:29 +0530

    Added code in bootstrap to create folder for storing files on file system if folder not created

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,14 @@
+import com.eads.threedviewer.CADObject
+
 class BootStrap {
 
     def bootStrapService
 
     def init = { servletContext ->
+        File file = CADObject.findOrCreateFolder()
+        if (!file.exists()) {
+            log.info "Folder for storing files not created at ${CADObject.folderPath}, it will cause application to misbehave."
+        }
     }
     def destroy = {
 
