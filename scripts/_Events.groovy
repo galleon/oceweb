090cccc862fa0056310d242dbb2f88d02678aaa2
Uday Pratap Singh <uday@intelligrape.com>
2012-05-28 19:07:33 +0530

    Added println of file path for version

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -2,4 +2,5 @@ eventCreateWarStart = {warName, File stagingDir ->
     Process process = "git log -1".execute()
     File file = new File("${stagingDir}/versionInfo.txt")
     file.text = process.getText()
+    println "Adding version info at ${stagingDir}/versionInfo.txt"
 }
